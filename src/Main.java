e21c469a7ac703591498ce4a97e293cba5ef9401
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-19 14:34:33 +0700

    ex 15.5.8 DONE the old way

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("1", "2", "3", "4", "5");
-        list
-                .stream()
-                .map(Integer::parseInt)
-                .reduce((x,y) -> x*y)
-                .ifPresent(System.out::println);
+        ArrayList<Integer> objects = new ArrayList<>();
+        for (var i : list){
+            objects.add(Integer.parseInt(i));
+        }
+        Iterator<Integer> iterator = objects.iterator();
+        int result = 1;
+        while (iterator.hasNext()){
+            result *= iterator.next();
+        }
+        System.out.println(result);
+
     }
 }
