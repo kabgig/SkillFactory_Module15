6fcac4a1aafd24cb1685655e2000beee1307e1b8
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-20 16:31:06 +0700

    ex 15.6.4 done

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,29 @@
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayDeque;
-import java.util.Iterator;
+import java.util.Deque;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         Writer writer = new FileWriter("/Users/ansarismagilov/Desktop/FileForWriting.txt");
-        BufferedReader br = new BufferedReader(new FileReader("/Users/ansarismagilov/Desktop/FileForWriting.txt"));
+        String line = "";
+        Deque<String> nums = new ArrayDeque<>();
 
         //creating file with numbers
         for (int i = 1; i < 10; i++) {
             var stream = Stream.iterate(1, x -> x + 1);
-            stream.limit(i).forEach(s -> {
-                try {
-                    writer.write(s.toString());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            writer.write("\n");
+            nums.addLast("\n");
+            stream.limit(i).forEach(s -> nums.addLast(s.toString()));
+        }
+
+        //writing
+        while((line = nums.pollLast()) != null){
+            writer.write(line);
+            System.out.print(line);
         }
         writer.close();
-
-        //reading
-        ArrayDeque<String> nums = new ArrayDeque<>();
-        String line = "";
-        while ((line = br.readLine()) != null) {
-            nums.addLast(line);
-        }
-        while((line = nums.pollLast()) != null){
-            System.out.println(line);
-        }
 
     }
 }
